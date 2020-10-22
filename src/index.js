61bd40450a37e27a90a390c8281c34c77637070b
YuhaoZhang-tirsky <tirsky@outlook.com>
2020-10-22 22:43:52 +1100

    Finalise home page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import { RoomProvider } from './context'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById('root')
 )
